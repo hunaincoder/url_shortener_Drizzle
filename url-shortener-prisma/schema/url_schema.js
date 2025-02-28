8fda2d8b1e2690529dfc2786aacc4b16b4ff2b45
Hunain Asif <127204688+hunaincoder@users.noreply.github.com>
2025-02-28 14:22:05 +0500

    Delete url-shortener-prisma directory

--- url-shortener-prisma/schema/url_schema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const urlSchema = mongoose.Schema({
-    id : {type : Number},
-    url: String,
-    shortcode: String,
-})
-
-export const urls = mongoose.model("url" , urlSchema);
