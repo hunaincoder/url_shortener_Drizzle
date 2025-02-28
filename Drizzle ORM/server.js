bc1e387558c25aa1a201b8c3b073dac033de3ccf
hunain <hunainasif2004@gmail.com>
2025-02-28 17:31:27 +0500

    added login and signup page

--- Drizzle ORM/server.js
+++ Drizzle ORM/server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/shorten", shortenerRoutes);
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
 
 try {
   // await connectDB();
